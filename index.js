f17042a0ac170b007bf9941da073f577851440ee
KuateGodlove <Kuategodlove379@gmail.com>
2026-02-17 13:55:35 +0100

    new

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ const publicRoutes = require('./Routes/public-routes');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || process.env.FRONTEND_URL || 'http://localhost:5174',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
